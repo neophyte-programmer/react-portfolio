e4c15118234a146d71a0fb65fcd36a38283c8289
Nutifafa Attor <attorfafa@gmail.com>
2022-05-04 14:56:53 +0000

    created index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Import ReactDOM from react-dom module
+import ReactDOM from "react-dom";
+
+// Render the App component into the DOM element with id 'root' in the index.html
+// file of the project folder and then run the application in the browser
+// The App component is defined in the src/App.js file
+// The App component is imported in the src/index.js file
+// The App component is rendered in the index.html file
+// The App component is the main component of the application
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
