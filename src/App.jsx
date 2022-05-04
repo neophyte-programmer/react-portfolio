3c67e2f85aeb6cb370b26536ae02b1c5caa55ca5
Nutifafa Attor <attorfafa@gmail.com>
2022-05-04 14:45:45 +0000

    created App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// The extension of the file is .jsx because it is a react component
+// Creating a functional component
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
