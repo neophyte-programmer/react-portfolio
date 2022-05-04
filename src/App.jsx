f8043bc9c9730d9ededf6e9ac643cc97aa2ed3e0
Nutifafa Attor <attorfafa@gmail.com>
2022-05-04 16:14:57 +0000

    imported components

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@
 // Creating a functional component
 
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    // <></> => This is an empty react fragment that is used to create a single element
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
