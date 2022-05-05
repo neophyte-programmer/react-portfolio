cf05acf10da23ea8ec6851b37ba7c027f0e74f51
Nutifafa Attor <attorfafa@gmail.com>
2022-05-05 18:22:07 +0000

    changed testing text

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,11 +6,6 @@ const Services = () => {
     <div>
       <h5 className="section__subtitle">What I Offer</h5>
       <h2 className="section__title">Services</h2>
-      <a href="#">This is a link</a> 
-      <br />
-      <p className="btn">button</p>
-      <br />
-      <p className="btn btn--primary">button</p>
 
     </div>
   )
