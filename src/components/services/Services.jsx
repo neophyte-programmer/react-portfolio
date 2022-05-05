2884c28ecc33f8fb602af7fbd127f15d4c3a6305
Nutifafa Attor <attorfafa@gmail.com>
2022-05-05 17:33:02 +0000

    testing classes

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,16 @@ import './services.css'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <h5 className="section__subtitle">What I Offer</h5>
+      <h2 className="section__title">Services</h2>
+      <a href="#">This is a link</a> 
+      <br />
+      <p className="btn">button</p>
+      <br />
+      <p className="btn btn--primary">button</p>
+
+    </div>
   )
 }
 
