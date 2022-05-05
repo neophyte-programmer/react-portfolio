45e476b0721665ba0a277cfee2f9d72006632a5d
Nutifafa Attor <attorfafa@gmail.com>
2022-05-05 21:08:42 +0000

    added btn classes

--- src/components/header/ActionButtons.jsx
+++ src/components/header/ActionButtons.jsx
@@ -4,8 +4,8 @@ import CV from './../../assets/cv.pdf' //import CV
 const ActionButtons = () => {
 	return (
 		<div className='action__buttons'>
-			<a href={CV} download>Download CV</a>
-            <a href='#contact'>Let's Talk</a>
+			<a href={CV} download className='btn'>Download CV</a>
+            <a href='#contact' className='btn btn--primary'>Let's Talk</a>
             
 		</div>
 	)
