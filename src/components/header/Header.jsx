690aa3ea751246bf19ef7e40656b4126f04ad589
Nutifafa Attor <attorfafa@gmail.com>
2022-05-05 21:01:50 +0000

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './header.css'
+import ActionButtons from './ActionButtons'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5 className="section__subtitle">Hello, I'm </h5>
+        <h1 className="section__title"> Nutifafa Afi Attor </h1>
+        <h5 className="section__subtitle text__light">Software Developer</h5>
+        <ActionButtons />
+
+      </div>
+
+    </header>
   )
 }
 
