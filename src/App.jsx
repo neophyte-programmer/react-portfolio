bdc098d7db64c4c889d11e413ca91af892d3051f
Nutifafa Attor <attorfafa@gmail.com>
2022-05-08 09:12:41 +0000

    imported testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -21,6 +22,7 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
+      <Testimonials />
       <Contact />
       <Footer />
     </>
