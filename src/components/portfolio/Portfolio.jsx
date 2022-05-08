7ea8fa40f85aa2c85e5c920973a2039b9876e88d
Nutifafa Attor <attorfafa@gmail.com>
2022-05-08 10:17:28 +0000

    using map method for portoflio generation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,64 +7,61 @@ import img4 from '../../assets/portfolio4.jpg'
 import img5 from '../../assets/portfolio5.png'
 import img6 from '../../assets/portfolio6.jpg'
 
-
 // Array to store portfolio details in an object and then map through the object to display the portfolio items
+// id is going to be the unique key for each portfolio item
 const data = [
-  {
-    id: 1,
-    image: img1,
-    title: 'Portfolio 1',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 2,
-    image: img2,
-    title: 'Portfolio 2',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 3,
-    image: img3,
-    title: 'Portfolio 3',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 4,
-    image: img4,
-    title: 'Portfolio 4',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 5,
-    image: img5,
-    title: 'Portfolio 5',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  },
-  {
-    id: 6,
-    image: img6,
-    title: 'Portfolio 6',
-    github: 'https://github.com/neophyte-programmer',
-    demo: 'https://dribbble.com/Alien_pixels'
-  }
+	{
+		id: 1,
+		image: img1,
+		title: 'Portfolio 1',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
+	{
+		id: 2,
+		image: img2,
+		title: 'Portfolio 2',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
+	{
+		id: 3,
+		image: img3,
+		title: 'Portfolio 3',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
+	{
+		id: 4,
+		image: img4,
+		title: 'Portfolio 4',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
+	{
+		id: 5,
+		image: img5,
+		title: 'Portfolio 5',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
+	{
+		id: 6,
+		image: img6,
+		title: 'Portfolio 6',
+		github: 'https://github.com/neophyte-programmer',
+		demo: 'https://dribbble.com/Alien_pixels',
+	},
 ]
 
-
-
-
 const Portfolio = () => {
-  return (
-    <section id="portfolio" className='section'>
-      <h5 className="section__subtitle">View My Recent Work</h5>
-      <h2 className="section__title">My Portfolio</h2>
+	return (
+		<section id='portfolio' className='section'>
+			<h5 className='section__subtitle'>View My Recent Work</h5>
+			<h2 className='section__title'>My Portfolio</h2>
 
-      <div className="container portfolio__container">
-        <article className="portfolio__item">
+			<div className='container portfolio__container'>
+				{/* <article className="portfolio__item">
           <div className="portfolio__img">
             <img src={img1} alt="" />
           </div>
@@ -75,11 +72,41 @@ const Portfolio = () => {
           <a href="https://dribbble.com/Alien_pixels" target="_blank" className="btn btn--primary">Demo</a>
          </div>
 
-        </article>
-       
-      </div>
-    </section>
-  )
+        </article> */}
+
+				{
+					// Mapping through the data array and displaying the portfolio items
+					data.map(({ id, image, title, github, demo }) => {
+						return (
+							<article key={id} className='portfolio__item'>
+								<div className='portfolio__img'>
+									<img src={image} alt='' />
+								</div>
+                <h3 className='portfolio__title'>{title}</h3>
+
+								<div className='portfolio__btns'>
+									<a
+										href={github}
+										target='_blank'
+										className='btn'
+									>
+										Github
+									</a>
+									<a
+										href={demo}
+										target='_blank'
+										className='btn btn--primary'
+									>
+										Demo
+									</a>
+								</div>
+							</article>
+						)
+					})
+				}
+			</div>
+		</section>
+	)
 }
 
 export default Portfolio
