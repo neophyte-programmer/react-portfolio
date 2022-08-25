c9c0eff3b52fa680f6775ee478ff8d49090412fa
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-25 13:27:41 +0000

    feat: :sparkles: changed links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {AiOutlineLinkedin, AiOutlineGithub, AiOutlineDribbble} from 'react-icons/ai'
+import {AiOutlineLinkedin, AiOutlineGithub, AiOutlineTwitter} from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
       <div className='header__socials'>
-          <a href="https://linkedin.com" target="_blank">
+          <a href="https://linkedin.com/in/neophyteprogrammer" target="_blank">
             <AiOutlineLinkedin className='header__social-icon'/>
           </a>
           <a href="https://github.com/neophyte-programmer" target="_blank">
             <AiOutlineGithub className='header__social-icon'/>
           </a>
-          <a href="https://dribbble.com" target="_blank">
-            <AiOutlineDribbble className='header__social-icon'/>
+          <a href="https://twitter.com/Nutifafa18" target="_blank">
+            <AiOutlineTwitter className='header__social-icon'/>
           </a>
 
       </div>
