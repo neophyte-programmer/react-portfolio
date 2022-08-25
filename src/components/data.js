19f1833b884126c8c5d42adf334c507a85194791
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-25 13:10:00 +0000

    feat: :sparkles: store data

---
+++ src/components/data.js
@@ -0,0 +1,30 @@
+import { alex, elvis, bentil } from "../assets/assets"
+
+export const testimonials = [
+    {
+		id: 1,
+		avatar: alex,
+		name: 'Alex Essilfie Benjamin',
+		review: 'As a fellow web developer, I\'ve worked alongside Afi and I can say she\'s a very winsome person who finds problem solving exhilarating especially in the field of TECH. She is a key team player and her skills. She managed me directly and I can say confidently that she is an invaluable team leader as well.',
+	},
+	{
+		id: 2,
+		avatar: bentil,
+		name: 'Shadrach Bentil',
+		review: 'For delivery and high quality web apps and designs, Fafa is the go-to developer. She pays attention to details and always delivers on time. I\'ve worked on a couple of projects with her and her presence in the team alone can not be trivialized. She is a very good team player.',
+	},
+	{
+		id: 3,
+		avatar: elvis,
+		name: 'Elvis Komla Agbesi',
+		review: 'From day one, I knew she was a star; a super fast learner. Never have I seen anyone progress this fast at this level in my life. She\'s at the top of her game.',
+	},
+	{
+		id: 4,
+		avatar: alex,
+		name: 'Kayla Mitchell',
+		review: 'What a helpful bunch. After being scammed by another web design company, I was recommended Fluid Studios and I now know why. They are quick to respond, informative and deliver what is asked for. Would highly recommend. Thanks guys!',
+	},
+]
+
+
